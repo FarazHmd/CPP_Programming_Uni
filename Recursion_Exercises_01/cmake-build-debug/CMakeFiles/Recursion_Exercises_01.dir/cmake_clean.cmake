67e67ad920766973c303cf697a55500035a83ab2
Mr_Hmd <89371191+FarazHmd@users.noreply.github.com>
2022-10-24 21:49:40 +0330

    Add files via upload

---
+++ Recursion_Exercises_01/cmake-build-debug/CMakeFiles/Recursion_Exercises_01.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Recursion_Exercises_01.dir/main.cpp.obj"
+  "Recursion_Exercises_01.exe"
+  "Recursion_Exercises_01.exe.manifest"
+  "Recursion_Exercises_01.pdb"
+  "libRecursion_Exercises_01.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Recursion_Exercises_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
