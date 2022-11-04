be402f374ea6c9e36d5040e77fd42912208a1f55
Mr_Hmd <89371191+FarazHmd@users.noreply.github.com>
2022-11-04 20:48:57 +0330

    Add files via upload

---
+++ Array_Exercise_02/cmake-build-debug/CMakeFiles/Array_Exercise_02.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Array_Exercise_02.exe"
+  "Array_Exercise_02.exe.manifest"
+  "Array_Exercise_02.pdb"
+  "CMakeFiles/Array_Exercise_02.dir/main.cpp.obj"
+  "libArray_Exercise_02.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Array_Exercise_02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
